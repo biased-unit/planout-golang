5d007c18384a6f6f1965137afc4c4361dc00a5a5
Brian Amadio <brian.amadio@stitchfix.com>
2021-09-02 13:07:06 -0700

    Compile outputs map[string]interface{}

--- compile.go
+++ compile.go
@@ -1,15 +1,27 @@
 package planout
 
 import (
+	"encoding/json"
+
 	"github.com/stitchfix/planout-golang/compiler"
 	"github.com/stitchfix/planout-golang/compiler/lexer"
 	"github.com/stitchfix/planout-golang/compiler/parser"
 )
 
-func Compile(code string) ([]byte, error) {
-	lx := lexer.New(code)
+func Compile(script string) (map[string]interface{}, error) {
+	lx := lexer.New(script)
 	ps := parser.New(lx)
 	comp := compiler.New(ps)
 
-	return comp.Run()
+	marshalled, err := comp.Run()
+	if err != nil {
+		return nil, err
+	}
+
+	var result map[string]interface{}
+	if err := json.Unmarshal(marshalled, &result); err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
